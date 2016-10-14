20ce7cb05936ff4018b0b9bc7cf710422c2095ed
Deon Marais <deon.marais@gmail.com>
2016-10-14 09:15:01 +0200

    Pizza FPS improved by reducing layout thrashing in the updatePositions function

---
+++ README2.md
@@ -0,0 +1,14 @@
+#Optomization
+
+Goal of the project is to Obtain a pagespeed score of at least 90 on the Portfolio page and increase fps while reducing Jank on the Pizza website.
+
+
+##Index page
+Pics resized (Perhaps a bit too much in the case of the Profilepic)
+
+CSS inlined
+
+
+##Pizza page
+
+###
--- views/js/main.js
+++ views/js/main.js
@@ -449,9 +449,11 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
+    var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[1], size);
+    var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+
     for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
     }
   }
@@ -497,15 +499,32 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
 // Moves the sliding background pizzas based on scroll position
+
+// number of Pizzas set to a lower yet sufficient number
+var backgroundPizzas= 33;
+
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+
+  var items = document.getElementsByClassName("mover");
+
+  var sTop =  document.body.scrollTop/1250;
+
+
+for (var i =  backgroundPizzas; i--;) {
+
+    var phase = Math.sin(sTop + (i % 5));
+
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+
   }
+
+
+
+
+
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
@@ -521,15 +540,23 @@ function updatePositions() {
 window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
+
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
+  //This sets the spacing of the elements
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+
+
+
+  for (var i = 0; i < backgroundPizzas; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza.png";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px";
+    elem.src = "images/pizza70x100.png";
+
+    //is this necessary?
+    //elem.style.height = "100px";
+    //elem.style.width = "73.333px";
+
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
